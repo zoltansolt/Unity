043e89f70a4d8cb76efbbe9c14a9741dcb7287b0
zoltan.solt <zoltan.solt@trendency.hu>
2019-07-10 15:24:38 +0200

    init

---
+++ Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("dead");
+                break;
+        }
+    }
+
+    void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+
+        float rotaionThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotaionThisFrame);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotaionThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
+    }
+}
---
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+
+    string[] level1Passwords = { "books", "aisle", "self", "password", "font" };
+    string[] level2Passwords = { "prisoner", "handcuffs", "holster", "uniform", "arrest" };
+    int level;
+
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+    string password;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu ()
+    {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        } else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        } else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        bool isValidLevelNumber = (input == "1" || input == "2");
+        if (isValidLevelNumber)
+        {
+            level = int.Parse(input);
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        switch(level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            default:
+                Terminal.WriteLine("Error");
+                Debug.LogError("Invalid level number");
+                break;
+        }
+        Terminal.WriteLine("Please enter your password, hint: " + password.Anagram());
+    }
+
+    // Update is called once per frame
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            DisplayWinScreen();
+        } else
+        {
+            Terminal.WriteLine("Wrong, try again!");
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
+
+    void ShowLevelReward()
+    {
+        switch(level)
+        {
+            case 1:
+                Terminal.WriteLine("Have a book...");
+                Terminal.WriteLine(@"
+    _________
+   /        //
+  /        //
+ /________//
+(________(/
+"
+                );
+                break;
+            case 2:
+                Terminal.WriteLine(@"
+         .::::::::::.        -(_)====u         .::::::::::.
+       .::::''''''::::.                      .::::''''''::::.
+     .:::'          `::::....          ....::::'          `:::.
+    .::'             `:::::::|        |:::::::'             `::.
+   .::|               |::::::|_ ___ __|::::::|               |::.
+   `--'               |::::::|_()__()_|::::::|               `--'
+    :::               |::-o::|        |::o-::|               :::
+    `::.             .|::::::|        |::::::|.             .::'
+     `:::.          .::\-----'        `-----/::.          .:::'
+       `::::......::::'                      `::::......::::'
+         `::::::::::'                          `::::::::::'
+
+"
+                );
+                break;
+            default:
+                Debug.LogError("Invalid level");
+                break;
+        }
+        
+    }
+}
